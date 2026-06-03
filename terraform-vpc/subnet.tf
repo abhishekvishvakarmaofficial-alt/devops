28692faeb4b5f4f56c504de827c2a7285ff9266a
anurag@gmail.com <abhishekvishvakarmaofficial@gmail.com>
2026-06-03 14:36:03 +0530

    initialising practical

---
+++ terraform-vpc/subnet.tf
@@ -0,0 +1,22 @@
+resource "aws_subnet" "public_1" {
+
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "ap-south-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-subnet-1"
+  }
+}
+
+resource "aws_subnet" "private_1" {
+
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "ap-south-1b"
+
+  tags = {
+    Name = "private-subnet-1"
+  }
+}
